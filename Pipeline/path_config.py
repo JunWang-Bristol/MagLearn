efc42003267c9176ecebbf1691a796554542ba74
Jingrong Yang <hr20631@alumni.bristol.ac.uk>
2025-05-21 16:26:10 +0100

    Update verification

--- Pipeline/path_config.py
+++ Pipeline/path_config.py
@@ -1,5 +1,5 @@
 # Define raw data path
-rawDataPath = r"C:\Users\1\Desktop\PHD\Maglearn\Github_MagLearn1\MagLearn\Example raw data\3C90"
+rawDataPath = r"C:\Users\1\Desktop\PHD\Maglearn\pre-training\pre-training\3C94"
 # Another example path
 # rawDataPath = r"C:\Users\1\Desktop\PHD\Maglearn\Github_MagLearn1\MagLearn\Example raw data\3C94"
 
